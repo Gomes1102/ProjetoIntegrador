baa94c319a68ce17a888c005a8251dce907b7a3a
Gomes1102 <matheusus68@gmail.com>
2026-03-11 16:54:27 -0300

    Add files via upload

---
+++ Tela1/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace Tela1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            label1 = new Label();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            button1 = new Button();
+            linkLabel1 = new LinkLabel();
+            linkLabel2 = new LinkLabel();
+            pictureBox1 = new PictureBox();
+            checkBox1 = new CheckBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(812, 627);
+            label1.Name = "label1";
+            label1.Size = new Size(160, 25);
+            label1.TabIndex = 0;
+            label1.Text = "Quantum && Code";
+            // 
+            // label2
+            // 
+            label2.Anchor = AnchorStyles.None;
+            label2.AutoSize = true;
+            label2.Location = new Point(452, 282);
+            label2.Name = "label2";
+            label2.Size = new Size(50, 15);
+            label2.TabIndex = 1;
+            label2.Text = "ACESSO";
+            // 
+            // textBox1
+            // 
+            textBox1.Anchor = AnchorStyles.None;
+            textBox1.BorderStyle = BorderStyle.None;
+            textBox1.Location = new Point(366, 300);
+            textBox1.Name = "textBox1";
+            textBox1.PlaceholderText = "Usúario";
+            textBox1.Size = new Size(223, 16);
+            textBox1.TabIndex = 2;
+            textBox1.Tag = "";
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // textBox2
+            // 
+            textBox2.Anchor = AnchorStyles.None;
+            textBox2.BorderStyle = BorderStyle.None;
+            textBox2.Location = new Point(366, 348);
+            textBox2.Name = "textBox2";
+            textBox2.PlaceholderText = "Senha";
+            textBox2.Size = new Size(223, 16);
+            textBox2.TabIndex = 3;
+            textBox2.UseSystemPasswordChar = true;
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.ActiveCaptionText;
+            button1.Cursor = Cursors.Hand;
+            button1.FlatAppearance.BorderSize = 4;
+            button1.FlatStyle = FlatStyle.Popup;
+            button1.ForeColor = SystemColors.ControlLightLight;
+            button1.Location = new Point(366, 395);
+            button1.Name = "button1";
+            button1.Size = new Size(223, 23);
+            button1.TabIndex = 4;
+            button1.Text = "ENTRAR";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // linkLabel1
+            // 
+            linkLabel1.Anchor = AnchorStyles.None;
+            linkLabel1.AutoSize = true;
+            linkLabel1.Location = new Point(484, 426);
+            linkLabel1.Name = "linkLabel1";
+            linkLabel1.Size = new Size(105, 15);
+            linkLabel1.TabIndex = 5;
+            linkLabel1.TabStop = true;
+            linkLabel1.Text = "Esqueceu a senha?";
+            // 
+            // linkLabel2
+            // 
+            linkLabel2.Anchor = AnchorStyles.None;
+            linkLabel2.AutoSize = true;
+            linkLabel2.Location = new Point(366, 426);
+            linkLabel2.Name = "linkLabel2";
+            linkLabel2.Size = new Size(69, 15);
+            linkLabel2.TabIndex = 6;
+            linkLabel2.TabStop = true;
+            linkLabel2.Text = "Cadastre-se";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(401, 159);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(150, 120);
+            pictureBox1.TabIndex = 8;
+            pictureBox1.TabStop = false;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(366, 370);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(92, 19);
+            checkBox1.TabIndex = 9;
+            checkBox1.Text = "Lembrar-me";
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            AutoSize = true;
+            BackColor = SystemColors.AppWorkspace;
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(984, 661);
+            Controls.Add(checkBox1);
+            Controls.Add(pictureBox1);
+            Controls.Add(linkLabel2);
+            Controls.Add(linkLabel1);
+            Controls.Add(button1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            DoubleBuffered = true;
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Form1";
+            Load += Form1_Load_1;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Button button1;
+        private LinkLabel linkLabel1;
+        private LinkLabel linkLabel2;
+        private PictureBox pictureBox1;
+        private CheckBox checkBox1;
+    }
+}
---
+++ Tela1/Form1.cs
@@ -0,0 +1,31 @@
+namespace Tela1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button s = new button();
+
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
